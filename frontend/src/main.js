8ecfb4d10cf7fd44406f1af80f6a528f8d288de6
Evan Buffet <evan.buffet@hotmail.fr>
2025-06-16 13:51:00 +0200

    wip

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { MotionPlugin } from '@vueuse/motion'
 
 // Import Font Awesome
 import '@fortawesome/fontawesome-free/css/all.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(MotionPlugin).mount('#app')
